5916d5a285458049b61cba9f0ef262c48ac882b3
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2019-01-26 01:28:26 -0600

    added isImmuteable, createRooms, and placeRoom functions

--- mccartney_patrick.assignment-1.01/generator.c
+++ mccartney_patrick.assignment-1.01/generator.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // For reference, spaces are rocks ' ', floors are periods '.', corridors are hashes '#',
 // upward staircase are less than '<', downward stairs are greater than '>'
 
-
-#define dungeonCols 80
-#define dungeonRows 21
-#define numRooms 6
-#define minRoomWidth 4
-#define minRoomHeight 3
+#define DUNGEONROWS 21
+#define DUNGEONCOLS 80
+#define NUMROOMS 6
+#define ROOMWIDTH 4
+#define ROOMHEIGHT 3
 
 void printDungeon(char** dungeon);
 char** initializeDungeon();
 void freeDungeon(char** dungeon);
+int isImmuteable(char value);
+void createRooms(char** dungeon);
+int placeRoom(int width, int height);
 
 int main(int argc, char* argv[]) {
     char** dungeon = initializeDungeon();
     printDungeon(*&dungeon);
+
+    srand(time(NULL));
 
     freeDungeon(*&dungeon);
     return 0;
@@ -26,7 +31,7 @@ int main(int argc, char* argv[]) {
 char** initializeDungeon() {
     char** dungeon;
     //allocate pointers to the rows of our dungeon
-    dungeon = malloc(dungeonRows * sizeof(char*));
+    dungeon = malloc(DUNGEONROWS * sizeof(char*));
 
     if (dungeon == NULL) {
         fprintf(stderr, "Dungeon is null - out of memory\n");
@@ -35,8 +40,8 @@ char** initializeDungeon() {
 
     //now allocate enough columns for each row
     int i = 0;
-    for (i = 0; i < dungeonRows; i++) {
-        dungeon[i] = malloc(dungeonCols * sizeof(char*));
+    for (i = 0; i < DUNGEONROWS; i++) {
+        dungeon[i] = malloc(DUNGEONCOLS * sizeof(char*));
         if (dungeon[i] == NULL) {
             fprintf(stderr, "Out of memory - allocating dungeolCols");
             exit(0);
@@ -44,26 +49,26 @@ char** initializeDungeon() {
     }
 
     int row, col = 0;
-    for (row = 0; row < dungeonRows; row++) {
-        for (col = 0; col < dungeonCols; col++) {
+    for (row = 0; row < DUNGEONROWS; row++) {
+        for (col = 0; col < DUNGEONCOLS; col++) {
             dungeon[row][col] = ' ';
         }
     }
-    for (row = 0; row < dungeonRows; row++) {
+    for (row = 0; row < DUNGEONROWS; row++) {
         dungeon[row][0] = '|';
-        dungeon[row][dungeonCols - 1] = '|';
+        dungeon[row][DUNGEONCOLS - 1] = '|';
     }
-    for (col = 0; col < dungeonCols; col++) {
+    for (col = 0; col < DUNGEONCOLS; col++) {
         dungeon[0][col] = '-';
-        dungeon[dungeonRows - 1][col] = '-';
+        dungeon[DUNGEONROWS - 1][col] = '-';
     }
     return dungeon;
 }
 
 void printDungeon(char** dungeon) {
     int row, col = 0;
-    for (row = 0; row < dungeonRows; row++) {
-        for (col = 0; col < dungeonCols; col++) {
+    for (row = 0; row < DUNGEONROWS; row++) {
+        for (col = 0; col < DUNGEONCOLS; col++) {
             printf("%c", dungeon[row][col]);
         }
         printf("\n");
@@ -72,8 +77,33 @@ void printDungeon(char** dungeon) {
 
 void freeDungeon(char** dungeon) {
     int i = 0;
-    for (i = 0; i < dungeonRows; i++) {
+    for (i = 0; i < DUNGEONROWS; i++) {
         free(dungeon[i]);
     }
     free(dungeon);
+}
+
+int isImmuteable(char value) {
+    if (value == '-' || value == '|') {
+        return 1;
+    }
+    return 0;
+}
+
+void createRooms(char** dungeon) {
+    int rooms = 0;
+    while (rooms < NUMROOMS) {
+        // TODO: replace width and height with random values
+
+        int width = ROOMWIDTH;
+        int height = ROOMHEIGHT;
+        if (!(placeRoom(width, height))) {
+            fprintf(stderr, "Failed to create a room");
+            exit;
+        }
+    }
+}
+
+int placeRoom(int width, int height) {
+
 }
