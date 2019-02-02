5a9df1933b4d03b651519ca93f1c4fdeafb899e9
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2019-02-02 01:34:12 -0600

    debugged saving file, got properly working hexdump

--- mccartney_patrick.assignment-1.02/main.c
+++ mccartney_patrick.assignment-1.02/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "generator.h"
 #include <stdlib.h>
+#include <string.h>
 
-void generate(struct material **dungeon, struct room rooms[MAXROOMS]);
+void generate(struct material **dungeon, struct room rooms[MAXROOMS], struct stair stairs[NUMSTAIRS]);
 int save(char* filepath, struct material **dungeon, struct room rooms[MAXROOMS], struct stair stairs[NUMSTAIRS]);
 int load(char* filepath, struct material **dungeon);
 
@@ -14,34 +15,32 @@ int main (int argc, char* argv[]) {
     struct material **dungeon = allocateDungeon();
     struct room rooms[MAXROOMS];
     struct stair stairs[NUMSTAIRS];
-    generate(dungeon, rooms);
-    placeStairs(dungeon, rooms, stairs);
+    generate(dungeon, rooms, stairs);
 
 
-    char* filepath = getenv("$HOME");
-    strcat(filepath, ".rgl327/dungeon");
-    printf("Filepath: %s", filepath);
+    char* filepath = getenv("HOME");
+    strcat(filepath, "/.rlg327/dungeon");
+    printf("Filepath: %s\n", filepath);
 
-    if (save(filepath, dungeon)) {
+    if (save(filepath, dungeon, rooms, stairs)) {
         return -1;
     }
 
-    printDungeon(dungeon);
 }
 
-void generate(struct material **dungeon, struct room rooms[MAXROOMS])
+void generate(struct material **dungeon, struct room rooms[MAXROOMS], struct stair stairs[NUMSTAIRS])
 {
     drawBlank(dungeon);
     createRooms(dungeon, rooms);
     createCooridors(dungeon, 0, rooms);
-    placeStairs(dungeon, rooms);
+    placeStairs(dungeon, rooms, stairs);
 }
 
 int save(char* filepath, struct material **dungeon, struct room rooms[MAXROOMS], struct stair stairs[NUMSTAIRS]) {
     uint8_t numUpStairs = 1;
     uint8_t numDownStairs = 1;
     FILE* f;
-    if (!(f = fopen(filepath, 'w'))) {
+    if ((f = fopen(filepath, "w")) == NULL) {
         fprintf(stderr, "Error opening file at %s, exiting\n", filepath);
         return -1;
     }
@@ -70,7 +69,7 @@ int save(char* filepath, struct material **dungeon, struct room rooms[MAXROOMS],
     //unsure if this will work or if we need to write each position 1 by 1
     uint8_t playerposition[2] = {0, 0};
     if (!(fwrite(&playerposition, sizeof(playerposition), 1, f) == 1)) {
-        fprintf(stderr, "Failed to write the size %li, %li to %s\n", playerposition[0], playerposition[1], filepath);
+        fprintf(stderr, "Failed to write the size %i, %i to %s\n", playerposition[0], playerposition[1], filepath);
         return -1;
     }
 
@@ -79,7 +78,7 @@ int save(char* filepath, struct material **dungeon, struct room rooms[MAXROOMS],
     for (row = 0; row < DUNGEONROWS; row++) {
         for (col = 0; col < DUNGEONCOLS; col++) {
             if (!(fwrite(&dungeon[row][col].hardness, sizeof(dungeon[row][col].hardness), 1, f) == 1)) {
-                fprintf(stderr, "Failed to write hardness %li %li to %s\n", row, col, filepath);
+                fprintf(stderr, "Failed to write hardness %i %i to %s\n", row, col, filepath);
                 return -1;
             }
         }
@@ -95,26 +94,26 @@ int save(char* filepath, struct material **dungeon, struct room rooms[MAXROOMS],
     }
 
     if (!(fwrite(&numUpStairs, sizeof(numUpStairs), 1, f) == 1)) {
-        fprintf(stderr, "Failed to write to numUpStairs %li to %s\n", numUpStairs, filepath);
+        fprintf(stderr, "Failed to write to numUpStairs %i to %s\n", numUpStairs, filepath);
         return -1;
     }
 
     //this implementation sucks but might work down the road nicely
     for (i = 0; i < numDownStairs; i++) {
         if (!(fwrite(&stairs[i], sizeof(stairs[i]), 1, f)) == 1) {
-        fprintf(stderr, "Failed to write to a down stair %li %li to %s\n", stairs[i].xpos, stairs[i].ypos, filepath);
+        fprintf(stderr, "Failed to write to a down stair %i %i to %s\n", stairs[i].xpos, stairs[i].ypos, filepath);
         }
     }
 
 
     if (!(fwrite(&numDownStairs, sizeof(numDownStairs), 1, f) == 1)) {
-        fprintf(stderr, "Failed to write to numDownStairs %li to %s\n", numDownStairs, filepath);
+        fprintf(stderr, "Failed to write to numDownStairs %i to %s\n", numDownStairs, filepath);
         return -1;
     }
 
     for (i = numDownStairs; i < numUpStairs + numDownStairs; i++) {
         if (!(fwrite(&stairs[i], sizeof(stairs[i]), 1, f)) == 1) {
-            fprintf(stderr, "Failed to write to an up stair %li %li to %s\n", stairs[i].xpos, stairs[i].ypos, filepath);
+            fprintf(stderr, "Failed to write to an up stair %i %i to %s\n", stairs[i].xpos, stairs[i].ypos, filepath);
         }
     }
 
