a527a339c75ac11e74cc0d0989ca05f9bfb368f1
Patrick McCartney <pmccart@iastate.edu>
2019-02-09 01:08:18 -0600

    added old files

---
+++ mccartney_patrick.assignment-1.02/generator.h
@@ -0,0 +1,252 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h> //for uint8_t, etc
+#include <math.h>
+#include <time.h>
+
+#define DUNGEONCOLS 80
+#define DUNGEONROWS 21
+#define MAXROOMS 8 // this is the maximum number of rooms
+#define ROOMDATA 4
+#define MINROOMWIDTH 4
+#define MINROOMHEIGHT 3
+#define ROOM_SIZE_X 0
+#define ROOM_SIZE_Y 1
+#define ROOM_POS_X 2
+#define ROOM_POS_Y 3
+#define NUMSTAIRS 2
+
+struct material
+{
+    char value;
+    uint8_t hardness;
+};
+
+struct room
+{
+    //1 byte each, 4 bytes total and in order they go xpos, ypos, width, height
+    uint8_t xpos;
+    uint8_t ypos;
+    uint8_t width;
+    uint8_t height;
+};
+
+struct stair {
+    uint8_t xpos;
+    uint8_t ypos;
+};
+
+void printDungeon(struct material **dungeon);
+struct material **allocateDungeon();
+void drawBlank(struct material **dungeon);
+void freeDungeon(struct material **dungeon);
+void createRooms(struct material **dungeon, struct room rooms[MAXROOMS]);
+int isImmuteable(char value);
+int isValid(char value);
+int canPlaceRoom(struct material **dungeon, int x, int y, int width, int height);
+void drawRoom(struct material **dungeon, int x, int y, int width, int height);
+void createCooridors(struct material **dungeon, int roomNum, struct room rooms[MAXROOMS]);
+void placeStairs(struct material **dungeon, struct room rooms[MAXROOMS], struct stair stairs[NUMSTAIRS]);
+
+struct material **allocateDungeon()
+{
+    struct material **dungeon;
+    //allocate pointers to the rows of our dungeon
+    dungeon = malloc(DUNGEONROWS * sizeof(struct material *));
+
+    if (dungeon == NULL)
+    {
+        fprintf(stderr, "Dungeon is null - out of memory\n");
+        exit(0);
+    }
+
+    //now allocate enough columns for each row
+    int i = 0;
+    for (i = 0; i < DUNGEONROWS; i++)
+    {
+        dungeon[i] = malloc(DUNGEONCOLS * sizeof(struct material *));
+        if (dungeon[i] == NULL)
+        {
+            fprintf(stderr, "Out of memory - allocating dungeolCols");
+            exit(0);
+        }
+    }
+    return dungeon;
+}
+
+void drawBlank(struct material **dungeon)
+{
+    int row, col = 0;
+    for (row = 0; row < DUNGEONROWS; row++)
+    {
+        for (col = 0; col < DUNGEONCOLS; col++)
+        {
+            dungeon[row][col].value = ' ';
+            dungeon[row][col].hardness = 0;
+        }
+    }
+    for (row = 0; row < DUNGEONROWS; row++)
+    {
+        dungeon[row][0].value = '|';
+        dungeon[row][0].hardness = 255;
+        dungeon[row][DUNGEONCOLS - 1].value = '|';
+        dungeon[row][DUNGEONCOLS - 1].hardness = 255;
+    }
+    for (col = 0; col < DUNGEONCOLS; col++)
+    {
+        dungeon[0][col].value = '-';
+        dungeon[0][col].hardness = 255;
+        dungeon[DUNGEONROWS - 1][col].value = '-';
+        dungeon[DUNGEONROWS - 1][col].hardness = 255;
+    }
+}
+
+void printDungeon(struct material **dungeon)
+{
+    int row, col = 0;
+    for (row = 0; row < DUNGEONROWS; row++)
+    {
+        for (col = 0; col < DUNGEONCOLS; col++)
+        {
+            printf("%c", dungeon[row][col].value);
+        }
+        printf("\n");
+    }
+}
+
+void freeDungeon(struct material **dungeon)
+{
+    int i = 0;
+    for (i = 0; i < DUNGEONROWS; i++)
+    {
+        free(dungeon[i]);
+    }
+    free(dungeon);
+}
+
+int isImmuteable(char value)
+{
+    if (value == '-' || value == '|')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int isValid(char value)
+{
+    if (isImmuteable(value) || value == '.' || value == '#' || value == '<' || value == '>')
+    {
+        return 0;
+    }
+    return 1;
+}
+
+int canPlaceRoom(struct material **dungeon, int x, int y, int width, int height)
+{
+    if ((x + width + 1) > DUNGEONCOLS || (y + height + 1) > DUNGEONROWS)
+    {
+        return 0;
+    }
+    int row, col = 0;
+    for (col = x; col < x + width; col++)
+    {
+        for (row = y; row < y + height; row++)
+        {
+            if (!(isValid(dungeon[row - 1][col - 1].value) && isValid(dungeon[row][col - 1].value) && isValid(dungeon[row - 1][col].value) && isValid(dungeon[row][col].value) && isValid(dungeon[row + 1][col].value) && isValid(dungeon[row][col + 1].value) && isValid(dungeon[row + 1][col + 1].value)))
+            {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+void createRooms(struct material **dungeon, struct room rooms[MAXROOMS])
+{
+    int numRooms = 0;
+    while (numRooms < MAXROOMS)
+    {
+        int width = MINROOMWIDTH + (rand() % 7 + 1);
+        int height = MINROOMHEIGHT + (rand() % 7 + 1);
+
+        int x = (rand() % DUNGEONCOLS) + 1;
+        int y = (rand() % DUNGEONROWS) + 1;
+        if (canPlaceRoom(dungeon, x, y, width, height))
+        {
+            rooms[numRooms].xpos = x;
+            rooms[numRooms].ypos = y;
+            rooms[numRooms].width = width;
+            rooms[numRooms].height = height;
+            numRooms++;
+
+            // could be refactored to draw all at once instead of one at a time
+            drawRoom(dungeon, x, y, width, height);
+        }
+    }
+}
+
+void drawRoom(struct material **dungeon, int x, int y, int width, int height)
+{
+    // Attempting to follow the column first standard
+    int col, row = 0;
+    for (col = x; col < x + width; col++)
+    {
+        for (row = y; row < y + height; row++)
+        {
+            dungeon[row][col].value = '.';
+        }
+    }
+}
+
+/*
+    A better implementation could keep track of which rooms were connected along the way
+    which would require checking which '.' belongs to which room and only connecting that room once
+
+    also adding a bit of randomness would be cool
+    */
+void createCooridors(struct material **dungeon, int roomNum, struct room rooms[MAXROOMS])
+{
+    // basic idea is to keep adding to x using the provided formula
+    // when the origin is equal to the destination then go to
+    // doing the same for the y values. Also, don't overrite rooms
+    int x = rooms[roomNum].xpos;
+    int y = rooms[roomNum].ypos;
+
+    int x1 = rooms[roomNum + 1].xpos;
+    int y1 = rooms[roomNum + 1].ypos;
+
+    while (x != x1)
+    {
+        x += (x1 - x) / abs(x1 - x);
+        if (dungeon[y][x].value == ' ')
+        {
+            dungeon[y][x].value = '#';
+        }
+    }
+
+    while (y != y1)
+    {
+        y += (y1 - y) / abs(y1 - y);
+        if (dungeon[y][x].value == ' ')
+        {
+            dungeon[y][x].value = '#';
+        }
+    }
+
+    if (roomNum < MAXROOMS - 2)
+    {
+        createCooridors(dungeon, roomNum + 1, rooms);
+    }
+}
+
+void placeStairs(struct material **dungeon, struct room rooms[MAXROOMS], struct stair stairs[NUMSTAIRS])
+{
+    uint8_t upX = (rand() % rooms[0].width) + rooms[0].xpos;
+    uint8_t upY = (rand() % rooms[0].height) + rooms[0].ypos;
+    dungeon[upY][upX].value = '<';
+
+    uint8_t downX = (rand() % rooms[MAXROOMS - 1].width) + rooms[MAXROOMS - 1].xpos;
+    uint8_t downY = (rand() % rooms[MAXROOMS - 1].height) + rooms[MAXROOMS - 1].ypos;
+    dungeon[downY][downX].value = '>';
+}
---
+++ mccartney_patrick.assignment-1.02/main.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include "generator.h"
+#include <stdlib.h>
+#include <string.h>
+
+void generate(struct material **dungeon, struct room rooms[MAXROOMS], struct stair stairs[NUMSTAIRS]);
+int save(char* filepath, struct material **dungeon, struct room rooms[MAXROOMS], struct stair stairs[NUMSTAIRS]);
+int load(char* filepath, struct material **dungeon);
+
+
+int main (int argc, char* argv[]) {
+    time_t seconds = time(0);
+    srand(seconds);
+    printf("seed: %ld\n", seconds);
+    struct material **dungeon = allocateDungeon();
+    struct room rooms[MAXROOMS];
+    struct stair stairs[NUMSTAIRS];
+    generate(dungeon, rooms, stairs);
+
+
+    char* filepath = getenv("HOME");
+    strcat(filepath, "/.rlg327/dungeon");
+    printf("Filepath: %s\n", filepath);
+
+    if (save(filepath, dungeon, rooms, stairs)) {
+        return -1;
+    }
+
+}
+
+void generate(struct material **dungeon, struct room rooms[MAXROOMS], struct stair stairs[NUMSTAIRS])
+{
+    drawBlank(dungeon);
+    createRooms(dungeon, rooms);
+    createCooridors(dungeon, 0, rooms);
+    placeStairs(dungeon, rooms, stairs);
+}
+
+int save(char* filepath, struct material **dungeon, struct room rooms[MAXROOMS], struct stair stairs[NUMSTAIRS]) {
+    uint8_t numUpStairs = 1;
+    uint8_t numDownStairs = 1;
+    FILE* f;
+    if ((f = fopen(filepath, "w")) == NULL) {
+        fprintf(stderr, "Error opening file at %s, exiting\n", filepath);
+        return -1;
+    }
+
+    // file marker to denote type of file
+    char* filetype = "RLG327-S2019";
+    if (!(fwrite(&filetype, sizeof(filetype), 1, f) == 1)) {
+        fprintf(stderr, "Failed to write to filetype %s to %s\n", filetype, filepath);
+        return -1;
+    }
+
+    uint32_t version = 0;
+    if (!(fwrite(&version, sizeof(version), 1, f) == 1)) {
+        fprintf(stderr, "Failed to write the version %d to %s\n", version, filepath);
+        return -1;
+    }
+
+    // MAXROOMS may change
+    uint32_t size = 12 + 4 + 4 + 2 + 1680 + (4 * MAXROOMS) + (numUpStairs * 2) + 1 + (numDownStairs * 2);
+    if (!(fwrite(&size, sizeof(size), 1, f) == 1)) {
+        fprintf(stderr, "Failed to write the size %d to %s\n", size, filepath);
+        return -1;
+    }
+
+
+    //unsure if this will work or if we need to write each position 1 by 1
+    uint8_t playerposition[2] = {0, 0};
+    if (!(fwrite(&playerposition, sizeof(playerposition), 1, f) == 1)) {
+        fprintf(stderr, "Failed to write the size %i, %i to %s\n", playerposition[0], playerposition[1], filepath);
+        return -1;
+    }
+
+
+    int row, col = 0;
+    for (row = 0; row < DUNGEONROWS; row++) {
+        for (col = 0; col < DUNGEONCOLS; col++) {
+            if (!(fwrite(&dungeon[row][col].hardness, sizeof(dungeon[row][col].hardness), 1, f) == 1)) {
+                fprintf(stderr, "Failed to write hardness %i %i to %s\n", row, col, filepath);
+                return -1;
+            }
+        }
+    }
+
+    int i;
+    for (i = 0; i < MAXROOMS; i++) {
+        //TODO make sure thos writes in correct order
+        if (!(fwrite(&rooms[i], sizeof(rooms[i]), 1, f) == 1)) {
+            fprintf(stderr, "Failed to write a room to %s\n", filepath);
+            return -1;
+        }
+    }
+
+    if (!(fwrite(&numUpStairs, sizeof(numUpStairs), 1, f) == 1)) {
+        fprintf(stderr, "Failed to write to numUpStairs %i to %s\n", numUpStairs, filepath);
+        return -1;
+    }
+
+    //this implementation sucks but might work down the road nicely
+    for (i = 0; i < numDownStairs; i++) {
+        if (!(fwrite(&stairs[i], sizeof(stairs[i]), 1, f)) == 1) {
+        fprintf(stderr, "Failed to write to a down stair %i %i to %s\n", stairs[i].xpos, stairs[i].ypos, filepath);
+        }
+    }
+
+
+    if (!(fwrite(&numDownStairs, sizeof(numDownStairs), 1, f) == 1)) {
+        fprintf(stderr, "Failed to write to numDownStairs %i to %s\n", numDownStairs, filepath);
+        return -1;
+    }
+
+    for (i = numDownStairs; i < numUpStairs + numDownStairs; i++) {
+        if (!(fwrite(&stairs[i], sizeof(stairs[i]), 1, f)) == 1) {
+            fprintf(stderr, "Failed to write to an up stair %i %i to %s\n", stairs[i].xpos, stairs[i].ypos, filepath);
+        }
+    }
+
+}
+
+
+
+
