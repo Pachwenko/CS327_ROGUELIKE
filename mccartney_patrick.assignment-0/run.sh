7e5c4391b216111a59ec73023ab208bcf5c8efe2
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2020-09-27 21:13:12 -0500

    update folders

--- mccartney_patrick.assignment-0/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-./sobel bigger_digger.pgm
-./sobel motorcycle.pgm
--- mccartney_patrick.assignment-1.06/c-cpp.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# renames all .c files to .cpp
-
-for f in *.c; do mv "$f" "${f%.c}.cpp"; done
